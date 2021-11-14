47d735c5bc008d461baa5d5759a28e2187878da5
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2021-11-14 23:05:58 +0600

    heroku deploy

--- index.js
+++ index.js
@@ -31,98 +31,98 @@ async function run() {
         console.log('Mongodb Connect successfully!');
 
         // GET API
-        // app.get('/packages', async (req, res) => {
+        app.get('/packages', async (req, res) => {
 
-        //     const cursor = packagesCollection.find({})
-        //     const packages = await cursor.toArray();
-        //     res.send(packages);
+            const cursor = packagesCollection.find({})
+            const packages = await cursor.toArray();
+            res.send(packages);
 
-        // })
+        })
 
         // GET Single Service
-        // app.get('/packages/:packageId', async (req, res) => {
-        //     const id = req.params.packageId;
-        //     console.log('getting specific package', id);
-        //     const query = { _id: ObjectId(id) };
-        //     const package = await packagesCollection.findOne(query);
-        //     res.json(package);
-        // })
+        app.get('/packages/:packageId', async (req, res) => {
+            const id = req.params.packageId;
+            console.log('getting specific package', id);
+            const query = { _id: ObjectId(id) };
+            const package = await packagesCollection.findOne(query);
+            res.json(package);
+        })
 
         // GET API - All Bookings
-        // app.get('/allBooking', async (req, res) => {
+        app.get('/allBooking', async (req, res) => {
 
-        //     const cursor = bookingCollection.find({})
-        //     const allBooking = await cursor.toArray();
-        //     res.send(allBooking);
+            const cursor = bookingCollection.find({})
+            const allBooking = await cursor.toArray();
+            res.send(allBooking);
 
-        // })
+        })
 
         // GET API - My Bookings / Find Specific user booking
-        // app.get('/myBooking', (req, res) => {
-        //     bookingCollection.find({ email: req.query.email })
-        //         .toArray((err, documents) => {
-        //             res.send(documents)
-        //         })
-        // })
+        app.get('/myBooking', (req, res) => {
+            bookingCollection.find({ email: req.query.email })
+                .toArray((err, documents) => {
+                    res.send(documents)
+                })
+        })
 
 
         // add package POST / INSERT API
-        // app.post('/addPackage', async (req, res) => {
-        //     const packageDetails = req.body;
-        //     console.log('hit the post api', packageDetails);
+        app.post('/addPackage', async (req, res) => {
+            const packageDetails = req.body;
+            console.log('hit the post api', packageDetails);
 
-        //     const result = await packagesCollection.insertOne(packageDetails);
-        //     console.log(result);
+            const result = await packagesCollection.insertOne(packageDetails);
+            console.log(result);
 
-        //     res.json(result);
+            res.json(result);
 
-        // })
+        })
 
         // add booking POST / INSERT API
-        // app.post('/addBooking', async (req, res) => {
-        //     const bookingDetails = req.body;
-        //     console.log('booking data added', bookingDetails);
+        app.post('/addBooking', async (req, res) => {
+            const bookingDetails = req.body;
+            console.log('booking data added', bookingDetails);
 
-        //     const result = await bookingCollection.insertOne(bookingDetails);
-        //     console.log(result);
+            const result = await bookingCollection.insertOne(bookingDetails);
+            console.log(result);
 
-        //     res.json(result);
+            res.json(result);
 
-        // })
+        })
 
 
         // DELETE API
-        // app.delete('/myBooking/:id', async (req, res) => {
-        //     const id = req.params.id;
+        app.delete('/myBooking/:id', async (req, res) => {
+            const id = req.params.id;
 
-        //     const query = { _id: ObjectId(id) };
-        //     const result = await bookingCollection.deleteOne(query);
-        //     res.json(result);
+            const query = { _id: ObjectId(id) };
+            const result = await bookingCollection.deleteOne(query);
+            res.json(result);
 
-        //     if (result.deletedCount === 1) {
-        //         console.log("Successfully deleted one document.");
-        //     } else {
-        //         console.log("No documents matched the query. Deleted 0 documents.");
-        //     }
-        // })
+            if (result.deletedCount === 1) {
+                console.log("Successfully deleted one document.");
+            } else {
+                console.log("No documents matched the query. Deleted 0 documents.");
+            }
+        })
 
 
         
 
         // UPDATE order 
-        // app.put('/allBooking/:id', (req, res) => {
-        //     const filter = { _id: ObjectId(req.params.id) }
-        //     bookingCollection.updateOne(filter, {
-        //         $set: {
-        //             currentStatus: 'Approved'
-        //         }
-        //     })
-        //         .then((result) => {
-        //             res.send(result)
-        //         })
-        // })
+        app.put('/allBooking/:id', (req, res) => {
+            const filter = { _id: ObjectId(req.params.id) }
+            bookingCollection.updateOne(filter, {
+                $set: {
+                    currentStatus: 'Approved'
+                }
+            })
+                .then((result) => {
+                    res.send(result)
+                })
+        })
 
-        .
+        
 
     }
     finally {
